dcd3181ec14ed4b0e5e87402d0a85a09e7300680
Lancelot Miserachs Gonzalez <75066588+Lancelot1991@users.noreply.github.com>
2023-12-08 12:04:32 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -49,3 +49,74 @@ window.addEventListener('DOMContentLoaded', async () => {
     icon2.src = "/icons/moon.png";
   }
 });
+
+
+//SLIDESHOW
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
+
+//Automatic slideshow
+let slideIndexAuto = 0;
+showSlidesAuto();
+function showSlidesAuto() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexAuto++;
+  if (slideIndexAuto > slides.length) {slideIndexAuto = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexAuto-1].style.display = "flex";  
+  dots[slideIndexAuto-1].className += " active";
+  setTimeout(showSlidesAuto, 3000); // Change image every 2 seconds
+}
+
+
+
+
+//ANIMATED SCROLL INTO VIEW
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('translate-animation');
+    }
+  });
+});
+
+//Get all items we want to animate
+const animatedItem = document.querySelectorAll('.animated, .link');
+// Loop over the elements and add each one to the observer
+animatedItem.forEach((element) => observer.observe(element));
