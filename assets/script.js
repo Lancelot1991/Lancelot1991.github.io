a8ba0aba80f5bcb1dbdda3648206e709dccc7d0f
Lancelot Miserachs Gonzalez <75066588+Lancelot1991@users.noreply.github.com>
2023-12-12 19:07:38 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,30 @@
 let lastScrollTop = 0;
 const header = document.getElementById("header-hide");
 const icon = document.getElementById("icon-wrap");
+const dropdownMenu = document.getElementById("dropdown-wrap");
 const subMenues = document.querySelectorAll(".sub-menu");
 
 window.addEventListener("scroll", () => {
   const scrollTop = window.scrollY || document.documentElement.scrollTop;
   //the header should stay when the dropdown is opened
-  // if (!dropdown.open) {
-  if (scrollTop > lastScrollTop && scrollTop > 500) {
-    // Scrolling down
-    header.style.top = `-${header.offsetHeight}px`;
-    //when dropdown, the icon's position is also fixed. This allows for a different trasition effects.
-    icon.style.top = `-${header.offsetHeight}px`;
-  } else {
-    // Scrolling up
-    header.style.top = "0";
-    icon.style.top = "0";
+  if (!dropdownMenu.classList.contains("display") || window.innerWidth > 1000) {
+    if (scrollTop > lastScrollTop && scrollTop > 500) {
+      // Scrolling down
+      header.style.top = `-${header.offsetHeight}px`;
+      //when dropdown, the icon's position is also fixed. This allows for a different trasition effects.
+      icon.style.top = `-${header.offsetHeight}px`;
+    } else {
+      // Scrolling up
+      header.style.top = "0";
+      icon.style.top = "0";
+    }
   }
-  // }
   lastScrollTop = scrollTop;
 });
+//OPEN DROPDOWN ON CLICK
+icon.onclick = function () {
+  dropdownMenu.classList.toggle("display");
+};
 
 //DISPLAY SUBMENU ON HOVER
 subMenues.forEach((subMenu) => {
@@ -60,34 +65,34 @@ icon1.onclick = function () {
   }
 };
 
-const icon2 = document.getElementById("light-btn2");
-icon2.onclick = function () {
-  const userPreferredLanguage = localStorage.getItem("lightMode");
-  if (userPreferredLanguage == "on") {
-    localStorage.setItem("lightMode", "off");
-    location.reload();
-    document.body.classList.add("night-mode");
-    icon1.src = "/icons/sun.png";
-    icon2.src = "/icons/sun.png";
-  } else {
-    localStorage.setItem("lightMode", "on");
-    location.reload();
-    document.body.classList.remove("night-mode");
-    icon1.src = "/icons/moon.png";
-    icon2.src = "/icons/moon.png";
-  }
-};
+// const icon2 = document.getElementById("light-btn2");
+// icon2.onclick = function () {
+//   const userPreferredLanguage = localStorage.getItem("lightMode");
+//   if (userPreferredLanguage == "on") {
+//     localStorage.setItem("lightMode", "off");
+//     location.reload();
+//     document.body.classList.add("night-mode");
+//     icon1.src = "/icons/sun.png";
+//     icon2.src = "/icons/sun.png";
+//   } else {
+//     localStorage.setItem("lightMode", "on");
+//     location.reload();
+//     document.body.classList.remove("night-mode");
+//     icon1.src = "/icons/moon.png";
+//     icon2.src = "/icons/moon.png";
+//   }
+// };
 
 window.addEventListener("DOMContentLoaded", async () => {
   const userPreferredLanguage = localStorage.getItem("lightMode");
   if (userPreferredLanguage == "on") {
     document.body.classList.add("night-mode");
     icon1.src = "/icons/sun.png";
-    icon2.src = "/icons/sun.png";
+    // icon2.src = "/icons/sun.png";
   } else if (userPreferredLanguage == "off") {
     document.body.classList.remove("night-mode");
     icon1.src = "/icons/moon.png";
-    icon2.src = "/icons/moon.png";
+    // icon2.src = "/icons/moon.png";
   }
 });
 
