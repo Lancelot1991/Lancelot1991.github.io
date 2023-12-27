49b89c9cad44c44e390afe19d8ac596a8333d126
Lancelot Miserachs Gonzalez <75066588+Lancelot1991@users.noreply.github.com>
2023-12-28 00:08:15 +0100

    Add files via upload

--- assets/script-hero.js
+++ assets/script-hero.js
@@ -2,47 +2,47 @@ let currentSlide = 0;
 let slideInterval;
 
 function showSlide(n) {
-    const slidesContainer = document.querySelector('.slides');
-    currentSlide = (n + slidesContainer.children.length) % slidesContainer.children.length;
-    const translateValue = 200 - 100 * currentSlide + '%';
-    slidesContainer.style.transform = 'translateX(' + translateValue + ')';
+  const slidesContainer = document.querySelector(".slides");
+  currentSlide = (n + slidesContainer.children.length) % slidesContainer.children.length;
+  const translateValue = 150 - 100 * currentSlide + "%";
+  slidesContainer.style.transform = "translateX(" + translateValue + ")";
 
-    updateDots();
-    resetSlideInterval();
+  updateDots();
+  resetSlideInterval();
 }
 
 function updateDots() {
-    const dots = document.querySelectorAll('.dot');
-    dots.forEach((dot, index) => {
-        if (index === currentSlide) {
-            dot.classList.add('active');
-        } else {
-            dot.classList.remove('active');
-        }
-    });
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot, index) => {
+    if (index === currentSlide) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
 }
 
 function resetSlideInterval() {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, 5000); // Change slide every 4 seconds
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, 5000); // Change slide every 4 seconds
 }
 
 function nextSlide() {
-    showSlide(currentSlide + 1);
+  showSlide(currentSlide + 1);
 }
 
 function prevSlide() {
-    showSlide(currentSlide - 1);
+  showSlide(currentSlide - 1);
 }
 
 resetSlideInterval(); // Initialize the interval
 
 // Handle manual navigation for each dot
-document.querySelectorAll('.dot').forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        currentSlide = index;
-        showSlide(index);
-    });
+document.querySelectorAll(".dot").forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    currentSlide = index;
+    showSlide(index);
+  });
 });
 
 // Initially show the first slide
